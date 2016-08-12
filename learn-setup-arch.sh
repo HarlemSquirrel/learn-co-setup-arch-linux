cc918b8c7a311230b54d9ba22925ef4dd0ce6953
HarlemSquirrel <harlemsquirrel@gmail.com>
2016-08-11 20:14:01 -0400

    use rvm instead of rbenv

--- learn-setup-arch.sh
+++ learn-setup-arch.sh
@@ -10,27 +10,46 @@ printf " Starting setup...\n"
 printf " Installing prerequisits...\n"
 sudo pacman -S git postgresql nodejs npm phantomjs chromium redis jdk8-openjdk icedtea-web java-openjfx
 
-printf " downloading from AUR...\n"
-wget -N -P ~/Downloads/ https://aur.archlinux.org/cgit/aur.git/snapshot/rbenv.tar.gz
-wget -N -P ~/Downloads/ https://aur.archlinux.org/cgit/aur.git/snapshot/ruby-build.tar.gz
 
-printf " extracting to /builds dir...\n"
-[ -e ~/builds ] || mkdir ~/builds
-tar -xuf ~/Downloads/rbenv.tar.gz -C ~/builds
-tar -xuf ~/Downloads/ruby-build.tar.gz -C ~/builds
+#
+## --- Pick rbenv OR rvm but DO not use both ruby version managers ---
+#
 
-printf " making and installing AUR packages(will need admin password)...\n"
-cd ~/builds/rbenv && makepkg -si
-cd ~/builds/ruby-build && makepkg -si
+### -- Use rbenv
+### https://github.com/rbenv/rbenv
+#printf " installing rbenv...\n"
+#printf " downloading from AUR...\n"
+#wget -N -P ~/Downloads/ https://aur.archlinux.org/cgit/aur.git/snapshot/rbenv.tar.gz
+#wget -N -P ~/Downloads/ https://aur.archlinux.org/cgit/aur.git/snapshot/ruby-build.tar.gz
 
-printf " setting up path.../n"
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-echo 'PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"' >> ~/.bashrc
+#printf " extracting to /builds dir...\n"
+#[ -e ~/builds ] || mkdir ~/builds
+#tar -xuf ~/Downloads/rbenv.tar.gz -C ~/builds
+#tar -xuf ~/Downloads/ruby-build.tar.gz -C ~/builds
 
-printf " installing and setting ruby 2.2.4...\n"
-rbenv install 2.2.4
-rbenv local 2.2.4
+#cd ~/builds/rbenv && makepkg -si
+#cd ~/builds/ruby-build && makepkg -si
+
+#printf " setting up path.../n"
+#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+#echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+#echo 'PATH="$(ruby -e 'print Gem.user_dir')/bin:$PATH"' >> ~/.bashrc
+
+#printf " installing and setting ruby 2.2.4...\n"
+#rbenv install 2.2.4
+#rbenv local 2.2.4
+
+
+### -- Use RVM
+### https://rvm.io/
+printf " installing rvm...\n"
+cd ~
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+
+printf " installing ruby 2.3.1 and setting as default...\n"
+rvm install 2.3.1 --default
+
 
 printf " setting rspec to run in color...\n"
 echo "--color" > ~/.rspec
